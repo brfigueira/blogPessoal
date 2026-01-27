17fe71b5364cb48da0a3c9125591bba25fd90332
Breno Dias <brdfigueira@gmail.com>
2026-01-27 08:37:51 -0300

    Atualizando projeto com front-end Angular

---
+++ blogPessoal-FrontEnd/src/app/core/models/usuario.model.ts
@@ -0,0 +1,6 @@
+export inteface Usuario {
+    id?: number;
+    nome: string;
+    email: string;
+    senha: string;
+}
---
+++ blogPessoal-FrontEnd/src/app/core/services/usuario.service.ts
@@ -0,0 +1,8 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environment } from '../../../environments/environments';
+import { Usuario } from '../models/usuario.model';
+
+@Injectable({
+    providedIn: 'root'
+})
