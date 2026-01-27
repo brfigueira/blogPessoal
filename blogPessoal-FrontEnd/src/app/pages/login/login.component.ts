dfcddf615031d77db1623768c82a82fad00fd5ac
Breno Dias <brdfigueira@gmail.com>
2026-01-27 11:53:31 -0300

    Iniciando o Login

---
+++ blogPessoal-FrontEnd/src/app/pages/login/login.component.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+    selector: 'app-login',
+    standalone: true,
+    imports: [CommonModule, FormsModule],
+    templateUrl: './login.component.html',
+    styleUrls: ['./login.component.css']
+})
+export class LoginComponent {
+
+    usuario = {
+        email: '',
+        senha: ''
+    };
+
+    login() {
+        console.log(this.usuario);
+    }
+}
