046f6cd186e344476eca1b11fe48596bd5a51c6d
Breno Dias <brdfigueira@gmail.com>
2026-01-28 10:47:40 -0300

    Atualizando aula

--- blogPessoal-FrontEnd/src/app/app.routes.ts
+++ blogPessoal-FrontEnd/src/app/app.routes.ts
@@ -1,7 +1,9 @@
 import { Routes } from '@angular/router';
 import { LoginComponent} from './pages/login/login.component';
+import { Cadastro } from './pages/cadastro/cadastro';
 
 export const routes: Routes = [
     {path: 'login', component: LoginComponent },
+    {path: 'cadastro', component: Cadastro},
     {path: '', redirectTo: 'login', pathMatch:'full'}
 ];
